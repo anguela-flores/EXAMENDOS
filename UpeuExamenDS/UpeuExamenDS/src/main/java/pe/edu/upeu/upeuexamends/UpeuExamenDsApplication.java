0cbd919f76099dd1a8b30e82fd2a593f5a106aed
anguela-flores <163874461+anguela-flores@users.noreply.github.com>
2024-06-10 22:29:36 -0500

    Add files via upload

---
+++ UpeuExamenDS/UpeuExamenDS/src/main/java/pe/edu/upeu/upeuexamends/UpeuExamenDsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.upeuexamends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpeuExamenDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpeuExamenDsApplication.class, args);
+	}
+
+}
